8f677b7eb46e71a3c48fc81d50efbdf5f67ac92d
CristianoSouza <cristianoantonio.souza10@gmail.com>
2018-10-04 17:11:26 -0300

    erro_triangulo

--- reconhecedorLLC.py
+++ reconhecedorLLC.py
@@ -57,30 +57,50 @@ def getNaoTerminal(simbolo):
 iterador = 1
 for palavra in palavras:
 	triangulo = []
+	triangulo.append([])
 	for i in range(0, len(palavra)):
 		lista = getNaoTerminal(palavra[i])
 		print('Lista obtida para o terminal ' + palavra[i] + ': ')
 		print(lista)
-		triangulo.append(lista)
+		triangulo[0].append(lista)
 
 	print('Triangulo obtido com a subtituicao dos terminais: ')
 	print(triangulo)
 
-	r= len(triangulo)-1
+	r= len(triangulo[0])-1
+	s= len(triangulo[0])
 	print(r)
-	for i in range(0, r):
-		print('Iteracao no triangulo: ' + str(i))
-		print('triangulo[i]: ')
-		print(triangulo[i])
-		print('triangulo[i+1]: ')
-		print(triangulo[i+1])
-		simbolo = triangulo[i][0] + triangulo[i+1][0]
-		print('Simbolo: ')
-		print(simbolo)
+	for j in range(1,s):
+		triangulo.append([])
+		for i in range(0, r-1):
+			print('Iteracao no triangulo: ' + str(i))
+			print('triangulo[i]: ')
+			print(triangulo[j-1][i])
+			print('triangulo[i+1]: ')
+			print(triangulo[j-1][i+1])
+			simbolo = triangulo[j-1][i][0] + triangulo[j-1][i+1][0]
+
+			print('Simbolo: ')
+			print(simbolo)
+
+			print(triangulo)
+			lista = getNaoTerminal(simbolo)
+			print('Lista obtida para ' + simbolo + ': ')
+			print(lista)
+			triangulo[j].append(lista)
+
+			#print('------------------')
+			#print( str(triangulo[0][0]))
+			#print( str(simbolo_inicial))
+			#print(str(triangulo[0][0]) == str(simbolo_inicial))
+			#print('------------------')
+		print('Triangulo apos a iteracao ' + str(j) + ': ')
+		print(triangulo)
 
 
+	print('Triangulo gerado, caso o simpobolo inicial esteja na posicao 0 do triangulo a palavra pertence a linguagem: ')
 	print('Instancia ' + str (iterador))
-	if (1==1):
+	if (triangulo[s][0][0] == simbolo_inicial):
 		print( palavra + ' e uma palavra valida')
 	else:
 		print( palavra + ' nao e uma palavra valida')
